607a681c584a12e3673ce6ab4de8241ebd5a8a4c
masalmon <maelle.salmon@yahoo.se>
2017-06-27 10:25:56 +0200

    improvements. poke @njtierney

--- code.R
+++ code.R
@@ -1,34 +1,54 @@
+# all available packages
 pkgs <- as.data.frame(available.packages())
+# their names
 package_names <- pkgs$Package
 
+# which ones have exactly one dot?
 dotted <- as.character(package_names[stringr::str_count(package_names, "\\.") == 1])
 length(dotted)
-length(dotted)/nrow(package_names)
+length(dotted)/length(package_names)
 
+# for each of these dotted packages find the potential origin name
+# in my theory it's the first thing before the dot
 get_origin <- function(name){
   stringr::str_split(name, "\\.", simplify = TRUE)[1]
 }
-source <- purrr::map_chr(dotted, get_origin)
+origin <- purrr::map_chr(dotted, get_origin)
 
-
-find_whether_source <- function(dotted, source){
-  is_a_package <- source %in% package_names 
+# the dotted packages is linked to an origin
+# if the origin is a package
+# if one of them is a dependency of the other
+find_whether_linked <- function(dotted, origin){
+  # is origin a package?
+  is_a_package <- origin %in% package_names 
   
-  dotted_imports <- dplyr::filter(pkgs, Package == dotted)$Imports
-  dotted_imports <- stringr::str_split(dotted_imports, ",", simplify = TRUE)
-  dotted_imports <- trimws(dotted_imports)
-  is_imported_by_dotted <- source %in% dotted_imports
+  dotted_info <- dplyr::filter_(pkgs, ~Package == dotted)
+  dotted_dependencies <- toString(c(as.character(dotted_info$Imports),
+                               as.character(dotted_info$Suggests),
+                               as.character(dotted_info$LinkingTo),
+                               as.character(dotted_info$Enhances),
+                               as.character(dotted_info$Depends)))
+  dotted_dependencies <- stringr::str_split(dotted_dependencies, ",", simplify = TRUE)
+  dotted_dependencies <- trimws(dotted_dependencies)
+  origin_imported_by_dotted <- origin %in% dotted_dependencies
   
-  source_imports <- dplyr::filter(pkgs, Package == source)$Imports
-  source_imports <- stringr::str_split(source_imports, ",", simplify = TRUE)
-  source_imports <- trimws(source_imports)
-  source_imports <- stringr::str_replace(source_imports, "\\(.*", "")
-  source_imports <- trimws(source_imports)
-  is_imported_by_source <- dotted %in% source_imports
+  origin_info <- dplyr::filter_(pkgs, ~Package == origin)
+  origin_dependencies <- toString(c(as.character(origin_info$Imports),
+                               as.character(origin_info$Suggests),
+                               as.character(origin_info$LinkingTo),
+                               as.character(origin_info$Enhances),
+                               as.character(origin_info$Depends)))
+  origin_dependencies <- stringr::str_split(origin_dependencies, ",", simplify = TRUE)
+  origin_dependencies <- trimws(origin_dependencies)
+  origin_dependencies <- stringr::str_replace(origin_dependencies, "\\(.*", "")
+  origin_dependencies <- trimws(origin_dependencies)
+  dotted_imported_by_origin <- dotted %in% origin_dependencies
   
-  is_a_package & (is_imported_by_dotted|is_imported_by_source)
+  is_a_package & (origin_imported_by_dotted|dotted_imported_by_origin)
 }
 
-which_sources <- purrr::map2_lgl(dotted, source, find_whether_source)
+which_origins <- purrr::map2_lgl(dotted, origin, find_whether_linked)
 
-data.frame(source = source, dotted = dotted)[which_sources,]
+data.frame(origin = origin, dotted = dotted)[which_origins,]
+
+length(which_origins) / length(dotted)
